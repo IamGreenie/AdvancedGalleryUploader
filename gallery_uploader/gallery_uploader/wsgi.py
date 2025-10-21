7eeb964c1370d1e09f11e945c84ce21d0783678b
Isaac Christensen <123215031+IamGreenie@users.noreply.github.com>
2025-10-20 19:48:23 -0600

    Update WSGI settings for gallery_uploader

--- gallery_uploader/gallery_uploader/wsgi.py
+++ gallery_uploader/gallery_uploader/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gallery_uploader.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gallery_uploader.gallery_uploader.settings')
 
 application = get_wsgi_application()
+
